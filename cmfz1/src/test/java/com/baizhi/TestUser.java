550f9a02fd33480ccd3e0fb704829a2f1021fbfe
sgsd-away <59362531+sgsd-away@users.noreply.github.com>
2020-01-02 16:44:09 +0800

    update 1.0

--- cmfz1/src/test/java/com/baizhi/TestUser.java
+++ cmfz1/src/test/java/com/baizhi/TestUser.java
@@ -28,6 +28,6 @@ public class TestUser {
 
     @Test
     public void test3() {
-        System.out.println("~~~~~~~~~~~~~~~~~~~~~");
+        System.out.println("*******************************");
     }
 }
